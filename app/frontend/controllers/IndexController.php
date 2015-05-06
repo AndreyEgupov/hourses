5e5d9c111d4969bd6ec0fcefa1d0f692fce07c01
sergey tkachenko <serg.tkachenko@hotmail.com>
2015-05-06 16:09:46 +0300

    fixed hot

--- app/frontend/controllers/IndexController.php
+++ app/frontend/controllers/IndexController.php
@@ -9,7 +9,28 @@ use Slider;
 class IndexController extends ControllerBase
 {
     public function indexAction() {
+        $currentPage = (int) @$_GET["page"];
 
+        $sql = "
+            SELECT p.*
+            FROM Products p
+            ORDER BY p.date_update, p.category_id
+        ";
+
+        $query  = $this->modelsManager->createQuery($sql);
+        $products   = $query->execute();
+        $paginator = new Model(
+            array(
+                "data" => $products,
+                "limit"=> ControllerBase::COUNT_ITEMS_VIEW,
+                "page" => $currentPage? $currentPage : 1
+            )
+        );
+        $page = $paginator->getPaginate();
+
+        $this->view->setVar('product', $page);
+
+        //$this->br->add($category->title, "category/list/".$categoryId);
     }
 
 }
